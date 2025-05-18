7ccb5644f5625eccd61bf4f3075b59e87d0b642b
smRid <omorsk911@gmail.com>
2025-05-18 19:13:48 +0600

    frontend setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='text-red-500 text-3xl font-bold underline'>
+      Hello world
+    </div>
+  )
+}
+
+export default App
