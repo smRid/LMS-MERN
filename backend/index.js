9a69091d75bdf44b1a1a5e1b439c36499b56b6e7
smRid <omorsk911@gmail.com>
2025-11-30 00:26:09 +0600

    backend setup

---
+++ backend/index.js
@@ -0,0 +1,23 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import connectDb from './config/connectDB.js'
+import cookieParser from 'cookie-parser'
+import authRouter from './route/authRoute.js'
+dotenv.config()
+
+const port = process.env.PORT
+const app = express()
+app.use(express.json())
+app.use(cookieParser())
+
+app.use("/api/auth", authRouter)
+
+
+app.get("/",(req,res)=>{
+    res.send("Hello from Server")
+})
+
+app.listen(port , ()=>{
+    console.log("Server Started")
+    connectDb()
+})
