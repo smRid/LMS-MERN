402626ec33673b23c88d4aed76322cdcba26d2e0
smRid <omorsk911@gmail.com>
2025-12-17 03:10:19 +0600

    add HomeCourses component and integrate it into Home page

---
+++ frontend/src/components/HomeCourses.jsx
@@ -0,0 +1,26 @@
+import homeCoursesStyles from "../assets/dummyStyles";
+import { coursesData } from "../assets/dummyHData";
+import { useNavigate } from "react-router-dom";
+import { Star } from "lucide-react";
+
+const HomeCourses = () => {
+    const navigate = useNavigate();
+    const { title, course: courseFont, detail } = homeCoursesStyles.fonts;
+    const visibleCourses = coursesData.slice(0, 8);
+    return (
+        <div className={homeCoursesStyles.container}>
+            <div className={homeCoursesStyles.mainContainer}>
+                <div className={homeCoursesStyles.header}>
+                    <h2 className={`${title} ${homeCoursesStyles.title}`}>
+                        <Star className={homeCoursesStyles.titleIcon} />
+                        <Star className={homeCoursesStyles.titleIcon} />
+                        Explore Top Courses
+                        <Star className={homeCoursesStyles.titleIcon} />
+                    </h2>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default HomeCourses;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import Navbar from '../components/Navbar'
 import Banner from '../components/Banner'
+import HomeCourses from '../components/HomeCourses'
 
 const Home = () => {
   return (
     <div>
         <Navbar />
         <Banner />
+        <HomeCourses />
     </div>
   )
 }
