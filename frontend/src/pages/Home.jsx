54a5995875de02cc9b2a15ca8c55028d59d34ea4
smRid <omorsk911@gmail.com>
2025-12-17 03:00:21 +0600

    update Home page to include Banner

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
 import Navbar from '../components/Navbar'
+import Banner from '../components/Banner'
 
 const Home = () => {
   return (
     <div>
         <Navbar />
+        <Banner />
     </div>
   )
 }
