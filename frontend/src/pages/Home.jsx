c064800a1026fa091fcfff482a16fa76dd6e8417
smRid <omorsk911@gmail.com>
2025-12-18 00:00:45 +0600

    add Footer component and integrate it into Home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import Navbar from '../components/Navbar'
 import Banner from '../components/Banner'
 import HomeCourses from '../components/HomeCourses'
 import Testimonial from '../components/Testimonial'
+import Footer from '../components/Footer'
 
 const Home = () => {
   return (
@@ -10,6 +11,7 @@ const Home = () => {
         <Banner />
         <HomeCourses />
         <Testimonial />
+        <Footer />
     </div>
   )
 }
