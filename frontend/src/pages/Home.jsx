a57eb30f6cbc92985a7b8829f31790a3d0cbd81b
smRid <omorsk911@gmail.com>
2025-12-17 22:54:54 +0600

    add Testimonial component and integrate it into Home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import Navbar from '../components/Navbar'
 import Banner from '../components/Banner'
 import HomeCourses from '../components/HomeCourses'
+import Testimonial from '../components/Testimonial'
 
 const Home = () => {
   return (
@@ -8,6 +9,7 @@ const Home = () => {
         <Navbar />
         <Banner />
         <HomeCourses />
+        <Testimonial />
     </div>
   )
 }
