367247bacdccafbb5d42076b25327eb92c1142b8
smRid <omorsk911@gmail.com>
2025-12-22 03:53:48 +0600

    Implement courseRouter routes

---
+++ backend/routes/courseRouter.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import multer from 'multer';
+import path from 'path';
+import { createCourse, deleteCourse, getCourseById, getCourses, getMyRating, getPublicCourses, rateCourse } from '../controllers/courseController.js';
+
+// MULTER SETUP
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => cb(null, path.join(process.cwd(), 'uploads')),
+  filename: (req, file, cb) => {
+    const unique = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    const ext = path.extname(file.originalname);
+    cb(null, `course-${unique}${ext}`);
+  },
+});
+
+const upload = multer({storage});
+
+const courseRouter = express.Router();
+
+courseRouter.get('/public', getPublicCourses);
+courseRouter.get('/', getCourses);
+courseRouter.get('/:id', getCourseById);
+
+courseRouter.post('/', upload.single('image'), createCourse);
+courseRouter.delete('/:id', deleteCourse);
+
+courseRouter.post('/:courseId/rate', rateCourse);
+courseRouter.get('/:courseId/rating', getMyRating);
+
+export default courseRouter;
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import 'dotenv/config';
 import { clerkMiddleware } from '@clerk/express'
 import connectDB from './config/db.js';
+import courseRouter from './routes/courseRouter.js';
 
 const app = express();
 const port = 4000;
@@ -14,7 +15,10 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(clerkMiddleware());
 
+app.use('/uploads', express.static('uploads'));
+
 // ROUTES
+app.use('/api/course', courseRouter);
 
 //Database Connection
 connectDB();
