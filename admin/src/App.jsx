63920986c7b92551f5afad6dc2879cc459baf090
smRid <omorsk911@gmail.com>
2025-12-27 04:23:45 +0600

    Add Bookings page and integrate BookingsPage component

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -2,12 +2,14 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Add from "./pages/Add";
 import List from "./pages/List";
+import Bookings from "./pages/Bookings";
 const App = () => {
   return (
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/addcourse" element={<Add />} />
         <Route path="/listcourse" element={<List />} />
+        <Route path="/bookings" element={<Bookings />} />
       </Routes>
   );
 };
---
+++ admin/src/pages/Bookings.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import BookingsPage from '../components/BookingsPage'
+
+
+const Bookings = () => {
+  return (
+    <div>
+        <Navbar />
+        <BookingsPage />
+    </div>
+  )
+}
+
+export default Bookings
